3e5354d7552c9b2e912ffe8bbdc8477e4fa47edd
Md-Thanveer <thanveerami612@gmail.com>
2024-09-11 18:44:18 +0530

    Update User.php
    add the return $this->hasMany(Mark::class);

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function marks()
+    {
+        return $this->hasMany(Mark::class);
+    }
+
 }
